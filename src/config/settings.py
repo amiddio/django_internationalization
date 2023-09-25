5c83fcea023c3f38a4d485f62d3d090f9c8e9015
d.shkinin <amiddio.dev@gmail.com>
2023-09-25 12:38:59 +0400

    Added app article

--- src/config/settings.py
+++ src/config/settings.py
@@ -39,12 +39,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django Apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local Apps
+    'article.apps.ArticleConfig',
 ]
 
 MIDDLEWARE = [
